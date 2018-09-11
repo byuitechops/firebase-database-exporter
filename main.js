a6b6b518f5097a6f64efccf5394e825bafb5d8a8
Sam McGrath <mcg13004@byui.edu>
2018-09-11 10:24:34 -0600

    Got it to properly work.

--- main.js
+++ main.js
@@ -1,17 +1,49 @@
+const COLLECTION = 'tool_logs';
+
 const admin = require('firebase-admin');
-const 
-const serviceAccount = require('./auth.json');
+const fs = require('file-system');
+const moment = require('moment');
+const auth = require('./auth.json');
+const filepath = `${__dirname}/backups`;
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(auth),
   databaseURL: "https://katana-24a36.firebaseio.com"
 });
 
-const ref = admin.firestore().collection('tool_logs').doc('0XgRpyLWtsKGuARzYZNK');
+const collectionRef = admin.firestore().collection(COLLECTION);
 
-var query = ref.get()
-               .then(snapshot => {
-                console.log(`Data: ${JSON.stringify(snapshot._fieldsProto.course_id)}`);
-               })
-               .catch(err => console.log(`Error: ${err}`));
+//TODO: remove backups folder to get updated copies
 
+async function createBackups() {
+  let offset = 0;
+  let stagger = true;
+
+  try {
+    while (stagger) {
+      console.log(`Retrieving...`);
+      let requests = await collectionRef.limit(500).offset(offset).get();
+
+      requests.forEach(request => {
+        offset++;
+        fs.writeFile(createName(request),
+          JSON.stringify(request.data()),
+          (err) => {
+            if (err) throw err;
+          });
+        // console.log(`${request.id} successfully written. Offset: ${offset}.`);
+      });
+      if (offset % 500 !== 0) stagger = false;
+
+      console.log((stagger) ? 'Batch completed.' : 'Backup completed.');
+    }
+  } catch (err) {
+    console.log(`Error: ${err}`);
+  }
+};
+
+function createName(snapshot) {
+  return `${filepath}/${snapshot.id} ${moment().format('ll')}.json`;
+}
+
+createBackups();
